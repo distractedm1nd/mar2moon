8bafe321d3b7c9103f745040616f395b597b84b4
Tristan <tristan-jacobs@hotmail.com>
2021-07-03 23:30:52 +0200

    updated main method header

--- scripts/CryptoSentimentAnalysis.py
+++ scripts/CryptoSentimentAnalysis.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from mar2moon.scripts import VideoDownloader
 
 DEFAULT_AUDIO_FILES_FOLDER = "data\\downloaded_audio_files"
 DEFAULT_CLIP_FOLDER = "data\\extracted_clips"
@@ -7,29 +8,44 @@ DEFAULT_CLIP_FOLDER = "data\\extracted_clips"
 DEFAULT_CLIP_LENGTH = 12  # in seconds
 
 
-def get_sentiments(video_urls=[], playlist_urls=[], coins=["BTC", "ETH", "DOGE"],
-                   wav2vec_model=None, sentiment_model=None,
+def get_sentiments(video_urls=[],
+                   playlist_urls=[],
+                   start_date=None,
+                   end_date=None,
+                   coins=["BTC", "ETH", "DOGE"],
                    audio_files_folder=DEFAULT_AUDIO_FILES_FOLDER,
                    clip_folder=DEFAULT_CLIP_FOLDER,
-                   clip_length=DEFAULT_CLIP_LENGTH):
+                   clip_length=DEFAULT_CLIP_LENGTH,
+                   wav2vec_model=None,
+                   sentiment_model=None,
+                   use_audio_features=True):
 
     """
     Gets sentiments for specified coins from audio/video files.
 
-    :param video_urls:
-    :param playlist_urls:
-    :param coins:
-    :param wav2vec_model:
-    :param sentiment_model:
-    :param audio_files_folder:
-    :param clip_folder:
-    :param clip_length:
+
+    :param video_urls: List of video/audio URLs to do download.
+    :param playlist_urls: List of playlist URLs to download.
+    :param start_date: Do not use videos/audios before this date.
+    :param end_date: Do not use videos/audios after this date.
+    :param coins: List of coins to find the sentiments for. Only BTC, ETH and DOGE are supported.
+    :param audio_files_folder: The folder where downloaded files are stored and the source folder of all audio files for
+     the analysis.
+    :param clip_folder: Extracted audio clips are stored in this folder.
+    :param clip_length: The length of each audio clip.
+    :param wav2vec_model: Trained speech to text model.
+    :param sentiment_model: Trained sentiment analysis model.
+    :param use_audio_features: Use audio features for sentiment labelling.
     :return: Returns a data frame with the following structure: (Date, Author, Title, Coin, Sentiment)
     """
 
     # Download audio
     if video_urls is not None or playlist_urls is not None:
-        download_audio_files(video_urls, playlist_urls, audio_files_folder)
+        download_audio_files(video_urls=video_urls,
+                             playlist_urls=playlist_urls,
+                             start_date=start_date,
+                             end_date=end_date,
+                             save_audio_files_folder=audio_files_folder)
 
     # Collect Audio Files (from audio_files_folder)
 
@@ -47,14 +63,27 @@ def get_sentiments(video_urls=[], playlist_urls=[], coins=["BTC", "ETH", "DOGE"]
     # Label sentiment
 
 
-
-def download_audio_files(video_urls=[], playlist_urls=[], save_audio_files_folder=DEFAULT_AUDIO_FILES_FOLDER):
+def download_audio_files(video_urls=[],
+                         playlist_urls=[],
+                         start_date=None,
+                         end_date=None,
+                         save_audio_files_folder=DEFAULT_AUDIO_FILES_FOLDER):
 
     # Download
 
-    # Ensure proper naming
+    ensure_correct_naming(save_audio_files_folder)
 
     pass
+
+
+def ensure_correct_naming(folder_path):
+    """Renames (downloaded) files to avoid problematic characters.
+
+    This function is executed after downloading videos to ensure correct naming of the files.
+    It can also be used to convert names of previously downloaded files.
+    """
+
+    VideoDownloader.ensure_correct_naming(folder_path)
 
 
 def clip_audio_files(source_audio_files_folder=DEFAULT_AUDIO_FILES_FOLDER,
